d27ffe9d5d1a36641cabb9c671ca3b872f6e99fa
menicacci <menicacci@outlook.com>
2022-05-14 19:10:12 +0200

    model

---
+++ Catering/src/main/java/it/uniroma3/siw/catering/model/Ingrediente.java
@@ -0,0 +1,62 @@
+package it.uniroma3.siw.catering.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Ingrediente {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+	
+	private String nome;
+	
+	private String origine;
+	
+	private String descrizione;
+	
+	public Ingrediente() {
+		this(null,null);
+	}
+	
+	public Ingrediente(String nome, String origine) {
+		this.nome = nome;
+		this.origine = origine;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getOrigine() {
+		return origine;
+	}
+
+	public void setOrigine(String origine) {
+		this.origine = origine;
+	}
+
+	public String getDescrizione() {
+		return descrizione;
+	}
+
+	public void setDescrizione(String descrizione) {
+		this.descrizione = descrizione;
+	}
+	
+}
